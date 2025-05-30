263ffaaf3d2d3410bf41e5aa4e8735f85c7b3574
Enigma-de <aayusharegmi2@gmail.com>
2025-05-30 10:32:43 +0545

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Header from '../../components/common/Header';
+import Footer from '../../components/common/Footer';
+import HeroSection from './ComponentHome/UI/HomeHeroSection';
+import SearchForm from './ComponentHome/SearchForm';
+import NowBookingSection from './ComponentHome/NowBookingSection';
+import BookingSteps from './ComponentHome/BookingSteps';
+import TopBusRoutes from './ComponentHome/TopBusRoutes';
+import TrendingOffers from './ComponentHome/TrendingOffers';
+import ServiceHighlights from './ComponentHome/ServiceHighlights';
+import AppPromotion from './ComponentHome/AppPromotion';
+import PaymentMethods from './ComponentHome/PaymentMethods';
+
+const HomePage = () => {
+  return (
+    <div className="bg-[#f5f5f5] min-h-screen">
+      <Header />
+      
+      <main>
+        <HeroSection />
+        <SearchForm />
+        <NowBookingSection />
+        <BookingSteps />
+        <TopBusRoutes />
+        <TrendingOffers />
+        <ServiceHighlights />
+        <AppPromotion />
+        <PaymentMethods />
+      </main>
+      
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
